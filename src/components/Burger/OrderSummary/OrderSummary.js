2c1458e0a1386ab5df7bfa93d6deaa8fbdda25f1
Linead96 <danielgarboza@yahoo.com>
2019-05-01 18:42:34 +0800

    Added logic for showing and hiding the Modal

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -5,9 +5,11 @@ import Aux from '../../../hoc/Auxilliary';
 const orderSummary = (props) => {
     const ingredientSummary = Object.keys(props.ingredients)
         .map(igKey => {
-            return <li key={igKey}>
+            return (
+                <li key={igKey}>
                     <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
                 </li>
+            );
         });
     
     return (
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -26,7 +26,8 @@ class BuilderBuilder extends Component {
       meat: 0,
     },
     totalPrice: 4,
-    purchaseable: false
+    purchaseable: false,
+    purchasing: false,
   };
 
   updatePurchaseState (ingredients) {
@@ -71,6 +72,10 @@ class BuilderBuilder extends Component {
     this.updatePurchaseState(updatedIngredients);
   }
 
+  purchaseHandler = () => {
+    this.setState({purchasing: true});
+  }
+
   render() {
     const disabledInfo = {
       ...this.state.ingredients
@@ -80,7 +85,7 @@ class BuilderBuilder extends Component {
     }
     return (
       <Aux>
-        <Modal>
+        <Modal show={this.state.purchasing}>
           <OrderSummary ingredients={this.state.ingredients} />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
@@ -89,6 +94,7 @@ class BuilderBuilder extends Component {
           ingredientRemoved={this.removeIngredientHandler}
           disabled={disabledInfo}
           purchaseable={!this.state.purchaseable}
+          ordered={this.purchaseHandler}
           price={this.state.totalPrice}
         />
       </Aux>
